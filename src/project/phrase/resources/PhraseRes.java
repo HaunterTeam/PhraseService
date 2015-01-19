b3177763a1691e234577e742009e78fe0304c167
luca-zamboni <lucazamboni92@gmail.com>
2015-01-19 15:59:54 +0100

    added ant

--- src/project/phrase/resources/PhraseRes.java
+++ src/project/phrase/resources/PhraseRes.java
@@ -31,26 +31,6 @@ public class PhraseRes {
 	@PersistenceContext(unitName = "introsde-jpa", type=PersistenceContextType.TRANSACTION)
 	private EntityManagerFactory entityManagerFactory;
         
-	/*
-	@GET
-    @Produces({ MediaType.APPLICATION_JSON})
-	public Phrase getPhrase(
-                @QueryParam("bmi") double bmi,
-                @QueryParam("bmiold") double bmiold,
-                @QueryParam("weathertype") int weatherType) {
-            
-            int change = 0;
-            if(bmiold != 0){
-                if(bmi > bmiold)
-                    change = 1;
-                else
-                    change = 2;
-            }
-            int bmiType = utils.BMIUtils.getLevelByBmi(bmi);
-            
-            return Phrase.getPhraseByWeatherAndByBmi(bmiType,change, weatherType);
-            
-	}*/
         
     @GET
     @Produces({ MediaType.APPLICATION_JSON})
@@ -61,7 +41,7 @@ public class PhraseRes {
                 @QueryParam("wt2") int wT2,
                 @QueryParam("wt3") int wT3) {
             
-            
+            System.err.println("ciao");
             int change = 0;
             if(bmiold != 0){
                 if(bmi > bmiold)
