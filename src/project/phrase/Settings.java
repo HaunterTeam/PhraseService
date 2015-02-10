747861d313956bc4e955523d7ea132579f6117c6
Roberto Zen <rob.zenn92@hotmail.it>
2015-02-10 21:29:39 +0100

    Settings updated

--- src/project/phrase/Settings.java
+++ src/project/phrase/Settings.java
@@ -7,7 +7,7 @@ public final class Settings {
 //	public static final String BASE_PORT = "5900";
 	
 	public static final String BASE_PROTOCOL = "http://";
-	public static final String BASE_URL = "localhost";
+	public static final String BASE_URL = "188.226.183.46";
 	public static final String BASE_PORT = "8010";
 	public static final String SERVICE_PATH = "/phrase-service/";
 	
